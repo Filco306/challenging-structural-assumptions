9770767736522d4d0bd7444e733dfb01cf870e88
Filip Cornell <filipcornell@MBPsomthorFilip.lan>
2022-10-02 21:28:27 +0200

    Hello

--- fewshot/FAAN/NELL/__init__.py
+++
@@ -1,1 +0,0 @@
-# Put all Nell data in this directory, including the dataset and pre-trained TransE embeddings.
--- fewshot/FAAN/wiki/__init__.py
+++
@@ -1,1 +0,0 @@
-# Put all Wiki data in this directory, including the dataset and pre-trained TransE embeddings.
